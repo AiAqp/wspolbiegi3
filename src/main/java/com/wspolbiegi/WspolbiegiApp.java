f7257ade7a573f3cee69e40c00508d07f124921d
Paweł Szetela <paweszetela@gmail.com>
2022-05-19 11:46:28 +0200

    wiekszy plik do testowania

--- src/main/java/com/wspolbiegi/WspolbiegiApp.java
+++ src/main/java/com/wspolbiegi/WspolbiegiApp.java
@@ -5,13 +5,15 @@ public class WspolbiegiApp {
     public static void main(String[] args) {
 
         CsvFile hw_csv = new CsvFile();
-        hw_csv.loadNewList("hw_25000.csv",1,25000);
+        hw_csv.loadNewList("hw_500000.csv",1,500000);
         hw_csv.printFile();
 
 
         CsvFileParallel hw_csv_p = new CsvFileParallel();
-        hw_csv_p.loadNewList("hw_25000.csv",1,25000);
+        hw_csv_p.loadNewList("hw_500000.csv",1,500000);
         hw_csv_p.printFile();
+
+
 
     }
 }
